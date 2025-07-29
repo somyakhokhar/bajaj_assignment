70b9c20063004787ba38b41b205a09322a396dd6
somyakhokhar <122541890+somyakhokhar@users.noreply.github.com>
2025-07-29 20:56:53 +0530

    Add files via upload

---
+++ SomyaKhokharApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomyaKhokharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomyaKhokharApplication.class, args);
+	}
+
+}
